b1b6640b6797a84f1986807ef8f7c9bab4f0b2f2
NatalyOC <noteroc@hotmail.com>
2018-03-20 23:11:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import ButtonGoogle from './components/ButtonGoogle';
+
+const App = () =>(
+  <ButtonGoogle/>
+  
+)
+
+export default App;
---
+++ src/components/ButtonGoogle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ButtonGoogle = () => (
+  <div className="container">
+    <div className="row">
+      <div className="col s12 center-align">
+        <input type="button" value="Login with Google" />
+      </div>
+    </div>
+  </div>  
+)
+
+export default ButtonGoogle;
