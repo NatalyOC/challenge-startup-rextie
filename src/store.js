7360c0551de90d07b0c7b06769535f8f421dc5f5
NatalyOC <noteroc@hotmail.com>
2018-03-22 17:36:49 -0500

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import Reducer from './reducer';
-
-const store= createStore(Reducer,
-// inyectamos la capacidad de usar Redux Dev Tools
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),);
-
-export default store;
